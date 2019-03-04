1de90c5af6c3e06cb2da3b66d9cf676aa1ee649f
tslight <tobyslight@gmail.com>
2019-03-04 17:18:57 +0000

    update help

--- nwbkup/__main__.py
+++ nwbkup/__main__.py
@@ -49,23 +49,25 @@ def getargs():
     Return a list of valid arguments. If no argument is given, default to $PWD.
     """
     parser = argparse.ArgumentParser(
-        description='Remove invalid characters from a given path.')
-    parser.add_argument("-c", "--csv",
+        description=("Backup network devices listed in [SOURCE] csv.\n"
+                     "Each device is backed up to [TFTP SERVER/PATH].\n"
+                     "Output success or failure to [DESTINATION] csv.\n"))
+    parser.add_argument("-s", "--source",
                         nargs='?',
                         default="./devices.csv",
                         type=chkfile,
                         help="a valid path to a csv file.")
-    parser.add_argument("-r", "--results",
+    parser.add_argument("-d", "--destination",
                         nargs='?',
                         default=".",
                         type=chkdir,
                         help="Path to store the results csv in.")
-    parser.add_argument("-s", "--server",
+    parser.add_argument("-t", "--tftp_server",
                         nargs='?',
                         default="10.1.0.6",
                         type=chkip,
                         help="TFTP IP or hostname to send the backups to.")
-    parser.add_argument("-p", "--path",
+    parser.add_argument("-p", "--tftp_path",
                         nargs='?',
                         default="/Network",
                         type=str,
@@ -78,17 +80,17 @@ def main():
     Main function
     """
     args = getargs()
-    targets = parse_csv(args.csv)
+    targets = parse_csv(args.source)
     if targets:
         pool = ThreadPool(8)
-        func = partial(connect_and_backup, args.server, args.path)
+        func = partial(connect_and_backup, args.tftp_server, args.tftp_path)
         results = pool.imap(func, targets)
         pool.close()
         pool.join()
 
     if results:
         results = parse_results(results)
-        write_log(results, args.results)
+        write_log(results, args.destination)
 
 
 if __name__ == '__main__':
